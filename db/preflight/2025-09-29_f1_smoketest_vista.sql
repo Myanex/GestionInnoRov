91b9a8821d5f51493c3fd6038f94b6b2c58b1361
Myanex <142110964+Myanex@users.noreply.github.com>
2025-09-29 10:28:25 -0300

    Create 2025-09-29_f1_smoketest_vista.sql

---
+++ db/preflight/2025-09-29_f1_smoketest_vista.sql
@@ -0,0 +1,316 @@
+[
+  {
+    "vcz_smoke": {
+      "dev": {
+        "total": 2,
+        "sample": [
+          {
+            "centro_id": "00000000-0000-0000-0000-000000000003",
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "00000000-0000-0000-0000-000000000011",
+                "rut": "11.111.111-1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000001",
+                "estado": "con_centro",
+                "nombre": "Piloto",
+                "centro_id": "00000000-0000-0000-0000-000000000003"
+              }
+            ],
+            "pilotos_total": 1,
+            "pilotos_con_centro": 1,
+            "pilotos_sin_centro": 0
+          },
+          {
+            "centro_id": null,
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "d7438cda-1768-403f-93d9-d9c0a78398d5",
+                "rut": "ROJ-e17fcdb6",
+                "ap_pat": "Rojas",
+                "codigo": "PIL-000005",
+                "estado": "con_centro",
+                "nombre": "Ana",
+                "centro_id": null
+              },
+              {
+                "id": "fe4a47e2-b34f-489d-8b2a-b3c29ba8d433",
+                "rut": "LEO-2e9d2eb8",
+                "ap_pat": "León",
+                "codigo": "PIL-000007",
+                "estado": "con_centro",
+                "nombre": "Beatriz",
+                "centro_id": null
+              },
+              {
+                "id": "13254ee7-ff12-4098-8cf9-492f83c5088e",
+                "rut": "AGU-b31fdccc",
+                "ap_pat": "Aguilar",
+                "codigo": "PIL-000006",
+                "estado": "sin_centro",
+                "nombre": "Felipe",
+                "centro_id": null
+              },
+              {
+                "id": "b44713a1-6e1f-40e3-9423-c46079e262e9",
+                "rut": "PPF1-a7e4a1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000004",
+                "estado": "con_centro",
+                "nombre": "Piloto Prueba F1",
+                "centro_id": null
+              },
+              {
+                "id": "c7fe3876-e006-4a86-97f4-8c06fce853a1",
+                "rut": "PPMIN-8a1831aa",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000008",
+                "estado": "con_centro",
+                "nombre": "Piloto Seed Min",
+                "centro_id": null
+              }
+            ],
+            "pilotos_total": 5,
+            "pilotos_con_centro": 4,
+            "pilotos_sin_centro": 1
+          }
+        ]
+      },
+      "admin": {
+        "total": 2,
+        "sample": [
+          {
+            "centro_id": "00000000-0000-0000-0000-000000000003",
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "00000000-0000-0000-0000-000000000011",
+                "rut": "11.111.111-1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000001",
+                "estado": "con_centro",
+                "nombre": "Piloto",
+                "centro_id": "00000000-0000-0000-0000-000000000003"
+              }
+            ],
+            "pilotos_total": 1,
+            "pilotos_con_centro": 1,
+            "pilotos_sin_centro": 0
+          },
+          {
+            "centro_id": null,
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "d7438cda-1768-403f-93d9-d9c0a78398d5",
+                "rut": "ROJ-e17fcdb6",
+                "ap_pat": "Rojas",
+                "codigo": "PIL-000005",
+                "estado": "con_centro",
+                "nombre": "Ana",
+                "centro_id": null
+              },
+              {
+                "id": "fe4a47e2-b34f-489d-8b2a-b3c29ba8d433",
+                "rut": "LEO-2e9d2eb8",
+                "ap_pat": "León",
+                "codigo": "PIL-000007",
+                "estado": "con_centro",
+                "nombre": "Beatriz",
+                "centro_id": null
+              },
+              {
+                "id": "13254ee7-ff12-4098-8cf9-492f83c5088e",
+                "rut": "AGU-b31fdccc",
+                "ap_pat": "Aguilar",
+                "codigo": "PIL-000006",
+                "estado": "sin_centro",
+                "nombre": "Felipe",
+                "centro_id": null
+              },
+              {
+                "id": "b44713a1-6e1f-40e3-9423-c46079e262e9",
+                "rut": "PPF1-a7e4a1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000004",
+                "estado": "con_centro",
+                "nombre": "Piloto Prueba F1",
+                "centro_id": null
+              },
+              {
+                "id": "c7fe3876-e006-4a86-97f4-8c06fce853a1",
+                "rut": "PPMIN-8a1831aa",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000008",
+                "estado": "con_centro",
+                "nombre": "Piloto Seed Min",
+                "centro_id": null
+              }
+            ],
+            "pilotos_total": 5,
+            "pilotos_con_centro": 4,
+            "pilotos_sin_centro": 1
+          }
+        ]
+      },
+      "centro": {
+        "total": 2,
+        "sample": [
+          {
+            "centro_id": "00000000-0000-0000-0000-000000000003",
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "00000000-0000-0000-0000-000000000011",
+                "rut": "11.111.111-1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000001",
+                "estado": "con_centro",
+                "nombre": "Piloto",
+                "centro_id": "00000000-0000-0000-0000-000000000003"
+              }
+            ],
+            "pilotos_total": 1,
+            "pilotos_con_centro": 1,
+            "pilotos_sin_centro": 0
+          },
+          {
+            "centro_id": null,
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "d7438cda-1768-403f-93d9-d9c0a78398d5",
+                "rut": "ROJ-e17fcdb6",
+                "ap_pat": "Rojas",
+                "codigo": "PIL-000005",
+                "estado": "con_centro",
+                "nombre": "Ana",
+                "centro_id": null
+              },
+              {
+                "id": "fe4a47e2-b34f-489d-8b2a-b3c29ba8d433",
+                "rut": "LEO-2e9d2eb8",
+                "ap_pat": "León",
+                "codigo": "PIL-000007",
+                "estado": "con_centro",
+                "nombre": "Beatriz",
+                "centro_id": null
+              },
+              {
+                "id": "13254ee7-ff12-4098-8cf9-492f83c5088e",
+                "rut": "AGU-b31fdccc",
+                "ap_pat": "Aguilar",
+                "codigo": "PIL-000006",
+                "estado": "sin_centro",
+                "nombre": "Felipe",
+                "centro_id": null
+              },
+              {
+                "id": "b44713a1-6e1f-40e3-9423-c46079e262e9",
+                "rut": "PPF1-a7e4a1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000004",
+                "estado": "con_centro",
+                "nombre": "Piloto Prueba F1",
+                "centro_id": null
+              },
+              {
+                "id": "c7fe3876-e006-4a86-97f4-8c06fce853a1",
+                "rut": "PPMIN-8a1831aa",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000008",
+                "estado": "con_centro",
+                "nombre": "Piloto Seed Min",
+                "centro_id": null
+              }
+            ],
+            "pilotos_total": 5,
+            "pilotos_con_centro": 4,
+            "pilotos_sin_centro": 1
+          }
+        ]
+      },
+      "oficina": {
+        "total": 2,
+        "sample": [
+          {
+            "centro_id": "00000000-0000-0000-0000-000000000003",
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "00000000-0000-0000-0000-000000000011",
+                "rut": "11.111.111-1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000001",
+                "estado": "con_centro",
+                "nombre": "Piloto",
+                "centro_id": "00000000-0000-0000-0000-000000000003"
+              }
+            ],
+            "pilotos_total": 1,
+            "pilotos_con_centro": 1,
+            "pilotos_sin_centro": 0
+          },
+          {
+            "centro_id": null,
+            "empresa_id": "00000000-0000-0000-0000-000000000001",
+            "pilotos_json": [
+              {
+                "id": "d7438cda-1768-403f-93d9-d9c0a78398d5",
+                "rut": "ROJ-e17fcdb6",
+                "ap_pat": "Rojas",
+                "codigo": "PIL-000005",
+                "estado": "con_centro",
+                "nombre": "Ana",
+                "centro_id": null
+              },
+              {
+                "id": "fe4a47e2-b34f-489d-8b2a-b3c29ba8d433",
+                "rut": "LEO-2e9d2eb8",
+                "ap_pat": "León",
+                "codigo": "PIL-000007",
+                "estado": "con_centro",
+                "nombre": "Beatriz",
+                "centro_id": null
+              },
+              {
+                "id": "13254ee7-ff12-4098-8cf9-492f83c5088e",
+                "rut": "AGU-b31fdccc",
+                "ap_pat": "Aguilar",
+                "codigo": "PIL-000006",
+                "estado": "sin_centro",
+                "nombre": "Felipe",
+                "centro_id": null
+              },
+              {
+                "id": "b44713a1-6e1f-40e3-9423-c46079e262e9",
+                "rut": "PPF1-a7e4a1",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000004",
+                "estado": "con_centro",
+                "nombre": "Piloto Prueba F1",
+                "centro_id": null
+              },
+              {
+                "id": "c7fe3876-e006-4a86-97f4-8c06fce853a1",
+                "rut": "PPMIN-8a1831aa",
+                "ap_pat": "Demo",
+                "codigo": "PIL-000008",
+                "estado": "con_centro",
+                "nombre": "Piloto Seed Min",
+                "centro_id": null
+              }
+            ],
+            "pilotos_total": 5,
+            "pilotos_con_centro": 4,
+            "pilotos_sin_centro": 1
+          }
+        ]
+      },
+      "centro_pick": "00000000-0000-0000-0000-000000000003",
+      "empresa_pick": "00000000-0000-0000-0000-000000000001"
+    }
+  }
+]
